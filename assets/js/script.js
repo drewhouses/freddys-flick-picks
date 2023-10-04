b46b5961a11b5ff3fd8fa06aa04a67fdffaf16aa
drewhouses <drewcasas@live.com>
2023-10-03 19:51:19 -0500

    added fucntionality to hide cover photo on button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,6 @@ var enterButtonEl = $(".cover-btn");
 
 enterButtonEl.on("click", function () {
   console.log("You clicked the button");
-  
+  $(".image-container").remove();
+  $("h1").removeClass("hide");
 });
