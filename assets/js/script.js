73a256babc122bd4e129028d0252dc43f38b922f
Jaysahhn <jasonmason96@yahoo.com>
2023-10-04 19:05:01 -0500

    Created audio for cover page and transition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
 var enterButtonEl = $(".cover-btn");
 
+//audio section
+var freddyCoverAudio = new Audio("thriller-ambient-14563.mp3");
+var freddyMainAudio = new Audio("I SHOULD WARN YOU PT2 - AUDIO FROM JAYUZUMI.COM.mp3");
+freddyCoverAudio.autoplay = true;
+freddyCoverAudio.play();
+// CURRENTLY USING THRILLER-AMBIENT & I SHOULD WARN YOU PT2 MP3
+
+// Ask Drew and Art about this error msg: DOMexception user didn't interact with document
+
+// cover page button
 enterButtonEl.on("click", function () {
-  console.log("You clicked the button");
-  $(".image-container").remove();
-  $(".main").removeClass("hide");
+  freddyCoverAudio.pause();
+  freddyMainAudio.play();
+  $(".main").removeClass("hide"); // try to get title to render asap
+  $(".image-container").fadeOut(1200);
 });
