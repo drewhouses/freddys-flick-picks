d5b835a488c34e15f66d6a7a8bc0800d09c71028
drewhouses <drewcasas@live.com>
2023-10-04 20:47:28 -0500

    Created greeting, prompt, and dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,11 @@ var enterButtonEl = $(".cover-btn");
 
 //audio section
 var freddyCoverAudio = new Audio("thriller-ambient-14563.mp3");
-var freddyMainAudio = new Audio("I SHOULD WARN YOU PT2 - AUDIO FROM JAYUZUMI.COM.mp3");
-freddyCoverAudio.autoplay = true;
-freddyCoverAudio.play();
+var freddyMainAudio = new Audio(
+  "I SHOULD WARN YOU PT2 - AUDIO FROM JAYUZUMI.COM.mp3"
+);
+// freddyCoverAudio.autoplay = true;
+// freddyCoverAudio.play();
 // CURRENTLY USING THRILLER-AMBIENT & I SHOULD WARN YOU PT2 MP3
 
 // Ask Drew and Art about this error msg: DOMexception user didn't interact with document
