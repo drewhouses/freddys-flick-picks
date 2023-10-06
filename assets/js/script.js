712a801d9d783a47c6bb1f6cd09832e16931ca62
Arturo <arturodamianvargas1996@gmail.com>
2023-10-05 20:55:34 -0500

    save changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,11 @@ var enterButtonEl = $(".cover-btn");
 
 //audio section
 var freddyCoverAudio = new Audio("thriller-ambient-14563.mp3");
-var freddyMainAudio = new Audio("I SHOULD WARN YOU PT2 - AUDIO FROM JAYUZUMI.COM.mp3");
-freddyCoverAudio.autoplay = true;
-freddyCoverAudio.play();
+var freddyMainAudio = new Audio(
+  "I SHOULD WARN YOU PT2 - AUDIO FROM JAYUZUMI.COM.mp3"
+);
+// freddyCoverAudio.autoplay = true;
+// freddyCoverAudio.play();
 // CURRENTLY USING THRILLER-AMBIENT & I SHOULD WARN YOU PT2 MP3
 
 // Ask Drew and Art about this error msg: DOMexception user didn't interact with document
@@ -13,6 +15,6 @@ freddyCoverAudio.play();
 enterButtonEl.on("click", function () {
   freddyCoverAudio.pause();
   freddyMainAudio.play();
-  $(".main").removeClass("hide"); // try to get title to render asap
-  $(".image-container").fadeOut(1200);
+  $(".main").removeClass("hide"); // try to get title to render   $(".image-container").fadeOut(1200);
 });
+
